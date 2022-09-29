de6e66919887a9d894fb6602d37044db5a84fab6
Victor Mayi <victormay.i.797@gmail.com>
2022-09-29 08:19:42 +0300

    completed assignment

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function About() {
+  return (
+    <React.Fragment>
+      <h1>This is my about component!</h1>
+    </React.Fragment>
+  );
+}
+
+export default About;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,53 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App"
 
-function Home() {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-}
 
-function App() {
-  return <Home />;
-}
+// function Home() {
+//   return (
+//     <div>
+//       <h1>Home!</h1>
+//     </div>
+//   );
+// }
+
+// function App() {
+//   return <BrowserRouter>
+//             <Route path="/">
+//               <Home />
+//             </Route>
+//             <Route path="/about">
+//               <About />
+//             </Route>
+//             <Route path="/login">
+//               <Login />
+//             </Route>
+//           </BrowserRouter>;
+// }
+
+// function About() {
+//   return (
+//     <div>
+//       <h1>This is my about component!</h1>
+//     </div>
+//   );
+// }
+
+// function Login() {
+//   return (
+//     <div>
+//       <h1>Login</h1>
+//       <form>
+//         <div>
+//           <input type="text" name="username" placeholder="Username" />
+//         </div>
+//         <div>
+//           <input type="password" name="password" placeholder="Password" />
+//         </div>
+//         <input type="submit" value="Submit" />
+//       </form>
+//     </div>
+//   );
+// }
 
 ReactDOM.render(<App />, document.getElementById("root"));
